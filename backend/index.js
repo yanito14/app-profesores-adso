716a5a5e84a0f9a06946bfc935a8c95b382a9bc5
yanitooo <9301268@alumnos.ufv.es>
2026-05-05 07:48:33 +0200

    index.js actualizado

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    if (req.method === 'OPTIONS') return res.sendStatus(204);
+    next();
+});
+
 // Rutas
 app.use('/asignaturas', require('./routes/asignaturas'));
 app.use('/inscripciones', require('./routes/inscripciones'));
